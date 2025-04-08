1036f7faed0b1dcb1f8794328127547d83396b97
lucasfelixdev <95368707+lucasfelixdev@users.noreply.github.com>
2025-04-08 20:05:37 -0300

    updates

--- script/reveal.js
+++ script/reveal.js
@@ -5,6 +5,34 @@ window.revelar = ScrollReveal({
 const duracao = 1200; // Define duração das animações
 const distancia = '80px';   // Define distância inicial da animação.
 
+    revelar.reveal('.fade-left', {
+        origin: 'left',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        opacity: 0,
+        interval: 300 // Aplica delay crescente automaticamente
+    });
+
+    revelar.reveal('.fade-letters-bt', {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        opacity: 0,
+        interval: 300 // Aplica delay crescente automaticamente
+    });
+
+
+    revelar.reveal('.fade-bottom-mobile', {
+        origin: 'top',
+        distance: '150px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        opacity: 0,
+        interval: 340 // Aplica delay crescente automaticamente
+    });
+
     revelar.reveal('.reveal-top', 
     {
     duration: duracao,
@@ -41,35 +69,17 @@ const distancia = '80px';   // Define distância inicial da animação.
         
     })
 
-    revelar.reveal('.reveal-right-delay0', 
+    // REVEAL DOS CARDS S-SERVICE
+    revelar.reveal('.reveal-right-cards', 
         {
             duration: duracao,
             distance: distancia,
             origin: 'right',
             easing: 'ease-in-out',
-            delay: 10
+            reveal: 20
             
     })
 
-    revelar.reveal('.reveal-right-delay1', 
-            {
-                duration: duracao,
-                distance: distancia,
-                origin: 'right',
-                easing: 'ease-in-out',
-                delay: 30
-                
-    })
-
-    revelar.reveal('.reveal-right-delay2', 
-                {
-                    duration: duracao,
-                    distance: distancia,
-                    origin: 'right',
-                    easing: 'ease-in-out',
-                    delay:150
-                    
-    })
     revelar.reveal('.spotlight', 
         {
             duration: duracao,
